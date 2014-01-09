95be4347afc826218c574910adf6ecdacc9190cf
xiangxw <xiangxw5689@126.com>
2014-01-09 10:27:23 +0800

    refine search word: do not remove numbers

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -80,7 +80,7 @@ static bool isSpecial(const QChar &ch)
     static const int len = strlen(chars);
 
     for (int i = 0; i < len; ++i) {
-        if (ch == chars[i] || ch.isNumber()) {
+        if (ch == chars[i]) {
             return true;
         }
     }
