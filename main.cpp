b3caa35a7758f426cfc56a11343737fbd49a7636
xiangxw <xiangxw5689@126.com>
2014-03-06 18:17:03 +0800

    v0.1.18

--- main.cpp
+++ main.cpp
@@ -19,7 +19,7 @@ int main(int argc, char *argv[])
     // set application info for QSettings
     a.setOrganizationName("ThinDict");
     a.setApplicationName("thindict");
-    a.setApplicationVersion("0.1.17");
+    a.setApplicationVersion("0.1.18");
 
     // create window
     MainWindow w;
